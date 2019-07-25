356cf94aa6b08293525bd2101f4acad3bf0eaf1f
sublimerocks86 <carl.h.lombardi@gmail.com>
2019-07-25 17:06:20 -0400

    updates

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# TriviaGame
+# New Jersey Devils Trivia (Repo - ) (Site - )
+This is a trivia game which tests knowledge of the New Jersey Devils. If it's happened on the ice or off since 1982 and it involves the Devils, this is fair game!
+
+## Why the Devils?
+As a Devils fan, I figured that a quiz that covered many aspects of the team history would be fun to create and informative. The look of the site is as basic as it comes, but that was done on purpose. The purpose of the game was not to be flashy, but rather create a working, fun app.
+
+## This site uses Javascript and JQuery 
+The basic build of the site was HTML/CSS with the use of Javascript to write the code of the game and the use of JQuery to manipulate the HTML. 
+
+## Features
+This page consists of an intro page, a 60 second quiz with running timer and 10 questions. Upon completion or the time running out, your score is tallied and then displayed. It's a challenge for even the most avid of fans, as the questions run the entirety of team history. 
+
+## Code Example
+The below code is for the countdown function. This code is a function with sets the clock moving, displays it using JQuery modified HTML, and notes that if the time left equals zero, there is an alert that the game is out of time and the game then is completed. 
+
+  countdown: function() {
+    game.timeleft--;
+    $("#timeleft-number").html(game.timeleft);
+    if (game.timeleft === 0) {
+      alert("You're Out Of Time!");
+      game.done();
+    }
+
+## Installation 
+No outside software is needed for installation. 
+
+## How to Use
+This is a pretty straight forward game. Upon hitting the button on the home page, the user is taken into the game where they have 60 seconds to answer 10 questions. Upon time being up or the hitting of the "I'm Finished" button, the user's results are shown on the screen. 
+
+## Issues and Solutions
+The biggest issue that I ran into in creation of this app was coming up with challenging questions and making the app work as intended. 
+
+## Credits
+This app was created, written and developed by Carl Lombardi. 
---
+++ assets/javascript/app.js
@@ -0,0 +1,120 @@
+var quiz = $("#gamespace"); 
+
+var questions = [
+    {
+        question: "Which Goalie leads the Devils in Career Wins?",
+        answers: ["Keith Kinkaid", "Martin Brodeur", "Chico Resch", "Sean Burke"],
+        rightanswer: "Martin Brodeur"
+    },
+    {
+        question: "Who won the Hart Trophy in 2017/2018?",
+        answers: ["Taylor Hall", "Jesper Bratt", "Will Butcher", "Kyle Palmieri"],
+        rightanswer: "Taylor Hall"
+    },
+    {
+        question: "Who scored the series winning goal vs New York in the 2012 playoffs?",
+        answers: ["Zach Parise", "Ilya Kovalchuk", "Adam Henrique", "Petr Sykora"],
+        rightanswer: "Adam Henrique"
+    },
+    {
+        question: "What was the Devils previous arena in New Jersey?",
+        answers: ["Prudential Center", "Continental Airlines Arena",  "Floyd Hall Arena", "Mennan Arena"],
+        rightanswer: "Continental Airlines Arena"
+    },
+    {
+        question: "What legendary Devil saw the number 26 retired in their honor?",
+        answers: ["Patrik Elias", "Brian Rolston", "Joe Cirella", "Jason Smith"],
+        rightanswer: "Patrik Elias"
+    },
+    {
+        question: "Which long time Devils player provides color commentary on MSG+?",
+        answers: ["Stephane Richer", "Bruce Driver", "Ken Daneyko" , "John Maclean"],
+        rightanswer: "Ken Daneyko"
+    },
+    {
+        question: "Who did the Devils play in the first regular season game at Prudential Center?",
+        answers: ["Toronto Maple Leafs", "New York Rangers", "Ottawa Senators", "Montreal Canadiens"],
+        rightanswer: "Ottawa Senators"
+    },
+    {
+        question: "Who is the only player to Captain the Devils to a Stanley Cup?",
+        answers: ["Scott Stevens", "Scott Niedermayer", "John Madden", "Aaron Broten"],
+        rightanswer: "Scott Stevens"
+    },
+    {
+        question: "Which Devils rival plays at Madison Square Garden?",
+        answers: ["Philadelphia Flyers", "New York Islanders", "Buffalo Sabres", "New York Rangers"],
+        rightanswer: "New York Rangers"
+    },
+    {
+        question: "Who was the Devils first Captain in New Jersey?",
+        answers: ["Steve Tambellini", "Don Lever", "Tapio Levo", "Yvon Vautour"],
+        rightanswer: "Don Lever"
+    }
+]
+
+var timer;
+
+var game = {
+  right: 0,
+  wrong: 0,
+  timeleft: 60,
+
+  countdown: function() {
+    game.timeleft--;
+    $("#timeleft-number").html(game.timeleft);
+    if (game.timeleft === 0) {
+      alert("You're Out Of Time!");
+      game.done();
+    }
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#box2").prepend(
+      "<h2>You Have <span id='timeleft-number'>60</span> Seconds Remaining!</h2>"
+    );
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      quiz.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        quiz.append("<input type='checkbox' name='question-" + i + "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    quiz.append("<button id='done'>I'm Finished!</button>");
+  },
+
+  done: function() {
+    var inputs = quiz.find("input:checked");
+    for (var i = 0; i < inputs.length; i++) {
+      if ($(inputs[i]).val() === questions[i].rightanswer) {
+        game.right++;
+      } else {
+        game.wrong++;
+      }
+    }
+    this.score();
+  },
+
+  score: function() {
+    clearInterval(timer);
+
+    $("#box2 h2").remove();
+
+    quiz.html("<h2>Here's Your game!</h2>");
+    quiz.append("<h3>Correct Responses: " + this.right + "</h3>");
+    quiz.append("<h3>Incorrect Responses: " + this.wrong + "</h3>");
+  }
+};
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
+
+$(document).on("click", "#done", function() {
+  game.done();
+});
